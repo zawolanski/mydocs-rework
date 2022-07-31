f82d571cd822e162087f80bc6aea1b32119a5f0a
Sebastian <sebastian.zawolanski@gmail.com>
2022-07-31 14:11:59 +0200

    feat: restructure files and add the tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => (
+  <div>
+    <p className="text-3xl font-bold underline p-10">Hello world!</p>
+  </div>
+);
+
+export default Home;
