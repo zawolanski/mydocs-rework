710a0bddccfdc05f3ee934ce46bcef44b004e598
Sebastian <sebastian.zawolanski@gmail.com>
2022-08-03 20:36:07 +0200

    feat: add a theme detection

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
+import { ThemeProvider } from 'next-themes';
 
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider attribute="class">
+    <Component {...pageProps} />
+  </ThemeProvider>
+);
 
 export default appWithTranslation(MyApp);
