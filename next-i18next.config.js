4a9f752f6fde90f2f08a7e2e7735520572e78260
Sebastian <sebastian.zawolanski@gmail.com>
2022-08-07 18:07:35 +0200

    chore: add reloadOnPrerender and debug flags to the i18next config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  debug: process.env.NODE_ENV === 'development',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'pl'],
   },
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
